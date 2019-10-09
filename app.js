f2f42ef59095ff42daa6effd17c3ab2343e00e06
Daesun Pak <daesunpak951@gmail.com>
2019-10-09 02:30:02 -0400

    Added submission feature for campgrounds. Added in bootstrap.

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require("express")
 var app = express()
 var port = 3000
+var bodyParser = require("body-parser")
 
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static("public"))
 
 app.set("view engine", "ejs")
@@ -10,22 +12,38 @@ app.get("/", function(req, res){
     res.render("home")
 })
 
+var campgrounds = [
+    {
+        name: "Salmon Creek",
+        image: "https://cdn.pixabay.com/photo/2016/11/29/04/17/bonfire-1867275_960_720.jpg"
+    },
+    {
+        name: "Granite Hill",
+        image: "https://cdn.pixabay.com/photo/2016/02/09/16/35/night-1189929_960_720.jpg"
+    },
+    {
+        name: "Mountain Goat's Rest",
+        image: "https://cdn.pixabay.com/photo/2017/08/17/08/08/camp-2650359_960_720.jpg"
+    }
+]
+
 app.get("/campgrounds", function(req, res){
-    var campgrounds = [
-        {
-            name: "Salmon Creek",
-            image: "https://cdn.pixabay.com/photo/2016/11/29/04/17/bonfire-1867275_960_720.jpg"
-        },
-        {
-            name: "Granite Hill",
-            image: "https://cdn.pixabay.com/photo/2016/02/09/16/35/night-1189929_960_720.jpg"
-        },
-        {
-            name: "Mountain Goat's Rest",
-            image: "https://cdn.pixabay.com/photo/2017/08/17/08/08/camp-2650359_960_720.jpg"
-        }
-    ]
-    res.render("campgrounds", {camgrounds: campgrounds})
+    res.render("campgrounds", {campgrounds: campgrounds})
+})
+
+app.get("/campgrounds/new", function(req, res){
+    res.render("newcamp")
+})
+
+app.post("/campgrounds", function(req, res){
+    var name = req.body.name
+    var image = req.body.image
+    var campObject = {
+        name: name,
+        image: image
+    }
+    campgrounds.push(campObject)
+    res.redirect("campgrounds")
 })
 
 app.listen(port, function(){
